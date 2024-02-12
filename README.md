c9b818ca36d14caba48619a8f887291035bc4e95
Chinweike99 <Innocentakwolu1@gmail.com>
2024-02-12 05:51:48 -0800

    To do list, stay organised

---
+++ README.md
@@ -0,0 +1,23 @@
+**ToDo List App**
+This is a simple yet powerful application that helps you organize your tasks and manage your daily activities effectively. With this app, you can easily add, remove, and mark tasks as completed, making it convenient to stay organized and productive.
+
+**Features**
+*Add tasks*: Enter a task in the input field and press Enter or click the Add button to add it to the list.
+Remove tasks: Click on the delete icon next to a task to remove it from the list.
+
+*Mark tasks as completed*: Click on a task to mark it as completed. Click again to unmark it.
+Getting Started
+
+*To use the ToDo List App, simply follow these steps:*
+
+Clone the repository to your local machine.
+Open the todo.html file in your web browser.
+Start adding tasks to the list and enjoy staying organized!
+Technologies Used
+HTML: Used to create the structure of the ToDo list app.
+CSS: Used to style the app and make it visually appealing.
+JavaScript: Used to add interactivity and functionality to the app.
+Demo
+
+**Contributing**
+Contributions are welcome! If you have any suggestions or ideas for improvement, feel free to open an issue or submit a pull request.
---
+++ todo.js
@@ -0,0 +1,51 @@
+const inputBox = document.querySelector("input");
+const listItem = document.querySelector("ul");
+const clickBtn = document.getElementById("clickBtn");
+
+
+function addTask(){
+    if (inputBox.value === ''){
+        alert("You cannot add empty task");
+    }
+    else{
+        let list = document.createElement("li");
+        list.innerHTML = inputBox.value;
+        listItem.appendChild(list);
+        
+        let cancl = document.createElement("span");
+        cancl.innerHTML = "\u00d7";
+        list.appendChild(cancl);
+        //listItem.append(document.createElement("br"));
+    }
+    inputBox.value = "";
+    saveList();
+}
+
+listItem.addEventListener("click", function(up_date){
+    if (up_date.target.tagName === "LI"){
+        up_date.target.classList.toggle("checked");
+        saveList();
+    }
+    else if(up_date.target.tagName === "SPAN"){
+        up_date.target.parentElement.remove();
+        saveList();
+    }
+});
+
+inputBox.addEventListener('keydown', function(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+        addTask(inputBox.value);
+    }
+})
+
+// Function to save created list
+function saveList(){
+    localStorage.setItem("data", listItem.innerHTML);
+}
+
+// Function to save List on website
+function webStore(){
+    listItem.innerHTML = localStorage.getItem("data");
+}
+webStore();
